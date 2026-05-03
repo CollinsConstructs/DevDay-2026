a6f74cd49bed53b4d44bb3b7eaee06e1a300340e
CollinsO-ECS <Dal115512@utdallas.edu>
2026-05-03 17:44:31 -0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AppContext } from '../context/AppContext';
 import { useAuth } from '../context/AuthContext';
 import { logoutUser } from '../firebase/auth';
 

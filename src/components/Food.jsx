25e202b15c97f260df42011729196130811ec77f
CollinsO-ECS <Dal115512@utdallas.edu>
2026-05-03 17:28:00 -0500

    Fix setIsSearching not defined error in Food.jsx

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -8,12 +8,14 @@ const INITIAL_CATEGORIES = Object.keys(CATEGORY_PRICES);
 
 const Food = () => {
   const [foods, setFoods] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
  
  
   const { favorites, toggleFavorite } = useContext(AppContext);
 
   // Fetch initial menu from TheMealDB
   const fetchInitialFoods = async () => {
+    setIsSearching(true);
    
     try {
       const results = await Promise.all(
